7871c20554aa94d8bbbfd9634a238daff9dff2d9
LuccaVedovato <bl4ck.h4ppy@hotmail.com>
2025-09-04 05:41:57 -0300

    Corrigidos

--- atividade_04/Program.cs
+++ atividade_04/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número de 1 a 7: ");
+        string entrada = Console.ReadLine();
+        int dia = int.Parse(entrada);  //Conversão para o switch.
+
+
+        switch (dia)   //Variável "diaa" não existe.
+                       //Alterado para "(dia)".
+        {                                       //switch usa string, mas os case eram números int.
+            case 1:                             //Converter entrada para int.
+                Console.WriteLine("Domingo");
+                break;                          //Faltaram ";" ao final das instruções.
+            case 2:                             //Mistura de tipos, "2" é string.
+                Console.WriteLine("Segunda-feira");
+                break;
+            case 3:
+                Console.WriteLine("Terça-feira");
+                break;
+            case 4:
+                Console.WriteLine("Quarta-feira");
+                break;
+            case 5:
+                Console.WriteLine("Quinta-feira");
+                break;
+            case 6:
+                Console.WriteLine("Sexta-feira");
+                break;
+            case 7:
+                Console.WriteLine("Sábado");
+                break;
+            default:
+                Console.WriteLine("Número inválido");     //Faltou ";" no final. Colocado.
+                break;
+        }
+    }
+}
--- atividade_09/Program.cs
+++ atividade_09/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] matriz = new int[3, 3];
+int somaTotal = 0;
+int somaDiagonal = 0;
+int[] maiores = new int[3];
+
+for (int i = 0; i < 3; i++)
+{
+    maiores[i] = int.MinValue;          //Se todos os valores da linha forem negativos o 0 é identificado como maior
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write($"Valor [{i},{j}]: ");
+        int valor = int.Parse(Console.ReadLine());
+
+        matriz[i, j] = valor;
+        somaTotal += valor;
+
+        if (i == j)
+        {
+            somaDiagonal += matriz[i, i];       //Os valores da diagonal principal não correspondem
+        }
+
+        if (valor > maiores[i])
+        {
+            maiores[i] = valor;
+        }
+    }
+}
+
+Console.WriteLine($"Soma total: {somaTotal}");                        //Várias instruções Console.WriteLine sem ";" no final
+Console.WriteLine($"Soma diagonal principal: {somaDiagonal}");
+
+Console.WriteLine("Maiores por linha:");
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(maiores[i]);
+}
+
+int[,] transposta = new int[3, 3];
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        transposta[j, i] = matriz[i, j];        //Estava copiando a matriz original, agora foi corrigido
+    }
+}
+
+Console.WriteLine("Matriz transposta:");
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write(transposta[i, j] + " ");
+    }
+    Console.WriteLine();
+}
