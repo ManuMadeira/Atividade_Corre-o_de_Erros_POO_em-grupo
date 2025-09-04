3488660b646d787358804e6ec7d25e1cc865b51e
Manu <emmanuellymadeira@gmailcom>
2025-09-03 21:44:55 -0300

    Atividade 6
    Descobrindo os erros e corrigidos

--- atividade_06/Program.cs
+++ atividade_06/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //double total = "0"; // Tipagem E2 CS0029 valor entre ""
+        double total = 0;
+        while (total < 50)
+        {
+            Console.Write("Digite o valor a ser guardado: ");
+            //double valor = double.Parse(Console.ReadLine().Replace('.', ',')); //CS8602 - E12 - CultureInfo.InvariantCulture
+            double valor = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture); 
+            //total =+ valor;  // E1 - Sintaxe operador incorreto
+            total += valor; 
+            
+        }
+        Console.WriteLine($"Meta atingida! Total economizado: R$ {total.ToString("0.00",CultureInfo.InvariantCulture)}"); // E9 interpolaçaõ $ e da E12 formatação do total usando o ToString
+    }
+}
